715cb5b85bd821e2a5fcf3353d3f3f2ee99d7d1a
Isabella Costa <isabellacosta.js@gmail.com>
2025-09-09 16:20:22 -0300

    Contorno script inicial

--- contours.py
+++ contours.py
@@ -23,7 +23,7 @@ def showImages(imgsArray, titleArray, size, grid=(1,1)):
     # .ravel() array achatado
     # if criar mais de um subplot, axes será um array NumPy multidimensional. 
     # else (se criar apenas um subplot (o padrão), axes não será um array), np.array() garante que ele seja colocado dentro de um array NumPy
-    axes = axes.ravel() if isinstance(axes, np.ndaray) else np.array([axes])
+    axes = axes.ravel() if isinstance(axes, np.ndarray) else np.array([axes])
 
     if len(imgsArray) != len(titleArray):
         print("ERROR: O número de imagens e títulos deve ser o mesmo")
@@ -59,25 +59,73 @@ def showImages(imgsArray, titleArray, size, grid=(1,1)):
 
 
 # ENCONTRAR CONTORNOS DE UMA IMAGEM BINÁRIA ---------------------------------------------------------------------------------------------------------------------------------------
-img = cv2.imread('contours_ex1'.png)
+img = cv2.imread('imagens/contours_ex1.png')
 assert img is not None, "file could not be read, check with os.path.exists()"  # Verifica se a imagem foi carregada corretamente
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERTE a imagem BGR para escala de cinza
 ret, thresh = cv2.threshold(img_gray, 127, 255,0) # Aplica um threshould para binarizar a imagem
 
 """" 
 Gera os contornos e a hierarquia. 
-Contours é uma lista em Python de todos os contornos da imagem. Cada contorno individual é um array Numpy de coordenadas (x,y) dos pontos de contorno do objeto.
+Contours é uma lista de todos os contornos da imagem. Cada contorno individual é um array Numpy de coordenadas (x,y) dos pontos de contorno do objeto.
 Hierarquia é uma matriz que descreve a hierarquia dos contornos
-    *contornos, hierarquia = cv.findContours (thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)*
-        o primeiro [thresh] é a imagem de origem
+    *contornos, hierarquia = cv2.findContours (thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)*
+        o primeiro [thresh] é a imagem de origem/ linearizada
         o segundo [cv2.RETR_TREE] é o modo de recuperação de contornos 
-        o terceiro [cv.CHAIN_APPROX_SIMPLE] é o método de aproximação de contornos.  
-         cv.CHAIN_APPROX_SIMPLE remove todos os pontos redundantes e compacta o contorno, economizando memória
+        o terceiro [cv2.CHAIN_APPROX_SIMPLE] é o método de aproximação de contornos.  
+         cv2.CHAIN_APPROX_SIMPLE remove todos os pontos redundantes e compacta o contorno, economizando memória
 """
 
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  #testar se esssa aproximação é a melhor opção/ INTERPOLAÇÃO
+
 print(contours, hierarchy)
-# Apenas 1 contorno. Não há relações hierárquica( pai, filho, p´roximo, anterior)
+# Apenas 1 contorno. Não há relações hierárquica [Próximo, Anterior, Primeiro_Filho, Pai]
 # Contorno. Os valores -1 indicam a ausencia de outros contornos.
 
 
+# DESENHAR CONTORNO --------------------------------------------------------------------------------------------------------------------------------------------------
+
+'''
+A função cv2.drawContours é usada para desenhar os contornos encontrados em uma imagem.
+    - O primeiro argumento [img] é a imagem onde se quer que os contornos apareçam
+    - O segundo argumento [contours] é a lista de todos os contornos que encontrados com cv.findContours 
+    - O terceiro argumento, -1, significa que se deseja desenhar todos os contornos na lista. 
+        Se quisesse desenhar apenas um contorno específico, se coloca o indíce do retorno.
+    - O quarto argumento [(255, 0, 0)] define a o cor do contorno matplot(RGB)
+    - O quinto argumento [3] define a expessura do contorno em 3 pixels
+
+    cv2.drawContours	(InputOutputArray	image,
+                         InputArrayOfArrays	contours,
+                         int	contourIdx,
+                         const Scalar &	color,
+                         int	thickness = 1,
+                         int	lineType = LINE_8,
+                         InputArray	hierarchy = noArray(),
+                         int	maxLevel = INT_MAX,
+                         Point	offset = Point() )
+'''
+
+#cv2.drawContours(img, contours, -1, (255, 0, 0), 3) 
+        #não necessariamente scale gray, mas draw.contourns é comum ser colorida(nosso caso não é) 
+        #modifica a img original, ideal criar um cópia da img
+#showImages([img_gray, img], ["Original", "Exemplo Contorno"], size=(7,7), grid=(1,2)) #RGB 
+
+
+print(f"Total de contornos: {len(contours)}")
+
+#Criar uma lista de imagens com cada contorno desenhado separadamente
+contour_images = []
+titles = []
+
+for i, contour in enumerate(contours):
+    img_contour = np.zeros_like(img) #Cria uma imagem preta do mesmo tamanho da original
+    cv2.drawContours(img_contour, contours, i, (255,255,255), 3)
+    contour_images.append(img_contour)
+    titles.append(f"Contorno {i}")
+
+
+num_images = len(contours)
+grid_size = (2,4)
+showImages([img], ["Original"], size=(4,4), grid= (1,1))
+showImages(contour_images, titles, size=(15, num_images), grid=grid_size)
+
+print(hierarchy)
