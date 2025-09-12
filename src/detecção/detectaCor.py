26832b14fc5c75807768193e1c79eca68b275693
Isabella Costa <isabellacosta.js@gmail.com>
2025-09-12 12:12:45 -0300

    Teste espectro de cor

--- src/detecção/detectaCor.py
+++ src/detecção/detectaCor.py
@@ -7,17 +7,29 @@ def detectar_cor_especifica(frame, contour):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Define os limites (lower/upper) para a cor #2B0000 (Vermelho Escuro) em HSV ---  RGB(43, 0, 0) -> HSV(0, 255, 43)
-    lower_red = np.array([0, 100, 20])
-    upper_red = np.array([100, 250, 100])
+    # Faixa 1 (vermelhos mais baixos)
+    lower_red1 = np.array([0, 100, 20])
+    upper_red1 = np.array([10, 255, 100])
+    mask1 = cv2.inRange(hsv_frame, lower_red1, upper_red1)
 
-    # Os pixels dentro da faixa de cor definida
-    mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+    # Faixa 2 (vermelhos mais altos)
+    lower_red2 = np.array([170, 100, 20])
+    upper_red2 = np.array([180, 255, 100])
+    mask2 = cv2.inRange(hsv_frame, lower_red2, upper_red2)
+    
+    # Combina as duas máscaras de cor
+    mask_cor = mask1 + mask2
+
+    
+
 
     # Cria uma máscara para a forma do contorno detectado
     mascara_contorno = np.zeros(frame.shape[:2], dtype="uint8")
     cv2.drawContours(mascara_contorno, [contour], -1, 255, -1)
 
-    mascara_combinada = cv2.bitwise_and(mascara_contorno, mask)
+    # Os pixels dentro da faixa de cor definida
+    mascara_combinada = cv2.bitwise_and(mask_cor, mask_cor, mask=mascara_contorno)
+
 
     # Se houver pixels na máscara combinada, a cor foi encontrada dentro da forma
     if cv2.countNonZero(mascara_combinada) > 50:  # 50 pixels de tolerância
